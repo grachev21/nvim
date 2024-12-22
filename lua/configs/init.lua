79d675ef1fc1714092e3c8ec25c7a143a6f33388
grachev21 <grachev613@gmail.com>
2024-12-22 17:54:57 +0400

    marks add

--- lua/configs/init.lua
+++
@@ -1,8 +0,0 @@
--- custom/init.lua
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end,
-})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,22 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- init.lua или mappings.lua
+
+-- Настройка сочетаний клавиш для vim-bookmarks
+vim.g.bookmark_auto_save = 1
+vim.g.bookmark_auto_save_file = vim.fn.expand "~/.config/nvim/bookmarks"
+
+local map = vim.keymap.set
+
+-- Создание метки
+map("n", "<leader>ma", "<Plug>BookmarkToggle", { desc = "Toggle bookmark" })
+
+-- Переход к следующей метке
+map("n", "<leader>mn", "<Plug>BookmarkNext", { desc = "Go to next bookmark" })
+
+-- Переход к предыдущей метке
+map("n", "<leader>mp", "<Plug>BookmarkPrev", { desc = "Go to previous bookmark" })
+
+-- Просмотр всех меток
+map("n", "<leader>ml", "<Plug>BookmarkListAll", { desc = "Show all bookmarks" })
