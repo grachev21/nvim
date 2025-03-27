d5327dc5d8b0260a7fbac4c66484f575174b9dc1
grachev21 <grachev613@gmail.com>
2025-03-27 07:13:24 +0400

    edit to windows 10

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,44 +1,40 @@
--- Загружаем конфигурации по умолчанию для LSP, такие как lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
--- Подключаем модуль lspconfig для настройки LSP серверов
 local lspconfig = require "lspconfig"
 
--- Пример настройки LSP серверов
-local servers = { "html", "cssls", "pyright" } -- Список серверов LSP, которые будут настроены с конфигурацией по умолчанию
+local servers = { "html", "cssls" }
 
-local nvlsp = require "nvchad.configs.lspconfig" -- Подключаем конфигурации LSP из nvchad
+local nvlsp = require "nvchad.configs.lspconfig"
 
--- Настраиваем серверы LSP с конфигурацией по умолчанию
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach, -- Функция, вызываемая при присоединении клиента LSP
-    on_init = nvlsp.on_init, -- Функция, вызываемая при инициализации клиента LSP
-    capabilities = nvlsp.capabilities, -- Возможности клиента LSP
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
-lspconfig.pyright.setup {
+lspconfig.pylsp.setup {
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
   settings = {
-    python = {
-      pythonPath = "python", -- или путь к интерпретатору
-      analysis = {
-        typeCheckingMode = "basic",
+    pylsp = {
+      plugins = {
+        jedi = {
+          enabled = true,
+          extra_dummies = "",
+        },
       },
     },
   },
 }
 
--- Пример настройки отдельного сервера LSP, например, для TypeScript
 lspconfig.ts_ls.setup {
-  on_attach = nvlsp.on_attach, -- Функция, вызываемая при присоединении клиента LSP
-  on_init = nvlsp.on_init, -- Функция, вызываемая при инициализации клиента LSP
-  capabilities = nvlsp.capabilities, -- Возможности клиента LSP
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
 }
 
--- Настройка LSP для Tailwind CSS
 lspconfig.tailwindcss.setup {
   on_attach = require("nvchad.configs.lspconfig").on_attach,
   on_init = require("nvchad.configs.lspconfig").on_init,
@@ -58,4 +54,3 @@ lspconfig.tailwindcss.setup {
     },
   },
 }
-
